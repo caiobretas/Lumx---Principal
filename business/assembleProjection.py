3416a050cbc7d92053ca0e76ea0bae47b76d2fbd
caiobretas <caiodinizbretas2001@gmail.com>
2023-04-25 17:44:17 -0300

    25/04/2023 17:44

--- business/assembleProjection.py
+++ business/assembleProjection.py
@@ -1,4 +1,9 @@
-
+from repositories.repositoryTransactions import RepositoryTransaction
 
 class AssembleProjection:
-    def __init__(self,
+    def __init__(self, connection, engine, schema, tableName):
+        self.repositoryTransactions = RepositoryTransaction(connection, engine, schema, tableName)
+        
+    def getData(self):
+        self.repositoryTransactions.getTransactions()
+        
--- entities/entitiyProjection.py
+++ entities/entitiyProjection.py
@@ -2,7 +2,7 @@ from uuid import UUID
 from datetime import datetime
 class Projection:
     def __init__(
-        self,id: UUID = None,data_lançamento: datetime = None,data_liquidação: datetime = None,datavencimento: datetime = None,valorprevisto: float = None,valorrealizado: float = None,moeda: str = None,cotação: str = None,totalBRL: float = None,realizado: bool = None,recorrente: bool = None,de: str = None,para: str = None,percentualrateio: float = None,nomecentrocusto: str = None,nomepessoa: str = None,observacao: str = None,descricao: str = None,numeronotafiscal: int = None,nomeprojeto: str = None,contaativo: str = None,subcategoria4: str = None,subcategoria3: str = None,subcategoria2: str = None,subcategoria: str = None,categoria: str = None,categoriaprojecao: str = None,categoriacusto_receita: str = None,hash: str = None,check_conciliadoorigem: bool = None,check_conciliadodestino: bool = None):
+        self,id: UUID = None,data_lançamento: datetime = None,data_liquidação: datetime = None,datavencimento: datetime = None,valorprevisto: float = None,valorrealizado: float = None,moeda: str = None,cotação: str = None, valorprevisto_BRL: float = None, valorrealizado_BRL: float = None,realizado: bool = None,recorrente: bool = None,de: str = None,para: str = None,percentualrateio: float = None,nomecentrocusto: str = None,nomepessoa: str = None,observacao: str = None,descricao: str = None,numeronotafiscal: int = None,nomeprojeto: str = None,contaativo: str = None,subcategoria4: str = None,subcategoria3: str = None,subcategoria2: str = None,subcategoria: str = None,categoria: str = None,categoriaprojecao: str = None,categoriacusto_receita: str = None,hash: str = None,check_conciliadoorigem: bool = None,check_conciliadodestino: bool = None):
         
         self.id = id
         self.data_lançamento = data_lançamento
@@ -12,7 +12,8 @@ class Projection:
         self.valorrealizado = valorrealizado
         self.moeda = moeda
         self.cotação = cotação
-        self.totalBRL = totalBRL
+        self.valorprevisto_BRL = valorprevisto_BRL
+        self.valorrealizado_BRL = valorrealizado_BRL
         self.realizado = realizado
         self.recorrente = recorrente
         self.de = de
