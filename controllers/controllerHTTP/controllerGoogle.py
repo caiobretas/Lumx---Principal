455b5dad80840c8fa4f6cacebd3f72a9b9677092
caiobretas <caiodinizbretas2001@gmail.com>
2023-05-09 15:03:30 -0300

    09/05/2023 - 15:03

--- controllers/controllerHTTP/controllerGoogle.py
+++ controllers/controllerHTTP/controllerGoogle.py
@@ -8,8 +8,29 @@ from controllers.controllerHTTP.controllerHTTPBase import ControllerHTTPBase
 class ControllerGoogle:
     def __init__(self):
         try:
-            creds = Credentials.from_service_account_file('credentials/credentialsGoogle.json')
-            credentials = ServiceAccountCredentials.from_json_keyfile_name(creds, ['https://www.googleapis.com/auth/spreadsheets'])
-            print(credentials)
+            credentialsPath = 'credentials/credentialsGoogle.json'
+            scopes = [
+                'https://www.googleapis.com/auth/spreadsheets'
+                      ]
+            self.credentials = ServiceAccountCredentials.from_json_keyfile_name(filename=credentialsPath, scopes=scopes)
+        except Exception as e:
+            logging.error(f'{" "* 3} Erro: {e}')
+            
+class GoogleSheets(ControllerGoogle):
+    def __init__(self):
+        super().__init__()
+        self.client = gspread.authorize(credentials=self.credentials)
+        
+    def updateWorksheet_byID(self, worksheetId: str, list_values: list[list], sheetName=None, start_row=2, start_col=1):
+        try:
+            worksheet = self.client.open_by_key(worksheetId).worksheet(sheetName)
+            headers = worksheet.row_values(1)
+            if not headers:
+                worksheet.insert_row(list(list_values[0].keys()), 1)
+            else:
+                list_values = list_values
+                range_str = f"{sheetName}!{chr(start_col + 64)}{start_row}:{chr(start_col + len(headers) - 1 + 64)}{len(list_values) + start_row - 1}"
+                worksheet.update(range_str, list_values)
+        
         except Exception as e:
             logging.error(f'{" "* 3} Erro: {e}')
