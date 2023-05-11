cc752624fda7349ab07f3e28f85df3d9d4d68745
caiobretas <caiodinizbretas2001@gmail.com>
2023-05-11 16:29:35 -0300

    11/05/2023 - 16:29

--- business/atualizaRepository.py
+++ business/atualizaRepository.py
@@ -17,10 +17,10 @@ class AtualizaFinanceRepository:
     def __init__(self, connFinance, engine, schema, pathIF, connProtocol) -> None:
         timer = time()
         
-        # UpdateBook(connection=connFinance, engine=engine, schema=schema, tableName='book', path_interface=pathIF,sheetName_interface='book')
-        # UpdateCategories(connection=connFinance, engine=engine,pathIF=pathIF,schema='finance', sheetName='categories', tableName='categories')
-        # UpdateCryptoTransactions(path_interface=pathIF, connection=connFinance, engine=engine, schema='finance', tableName='movements_crypto')
-        # UpdateCryptoPrices(connectionFinance=connFinance, engineAdmin=engine, schema=schema, tableName='prices_crypto')
+        UpdateBook(connection=connFinance, engine=engine, schema=schema, tableName='book', path_interface=pathIF,sheetName_interface='book')
+        UpdateCategories(connection=connFinance, engine=engine,pathIF=pathIF,schema='finance', sheetName='categories', tableName='categories')
+        UpdateCryptoTransactions(path_interface=pathIF, connection=connFinance, engine=engine, schema='finance', tableName='movements_crypto')
+        UpdateCryptoPrices(connectionFinance=connFinance, engineAdmin=engine, schema=schema, tableName='prices_crypto')
         UpdateTransactions(connection=connFinance,engine=engine,schema='finance',tableName='movements')
         UpdateFutures(connection=connFinance,engine=engine,schema='finance',tableName='movements')
 
