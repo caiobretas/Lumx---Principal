d60ddf55887e01db1bc191aeb28821a0b2af5fa2
Caio Bretas <caiodbretas@icloud.com>
2023-05-23 01:53:34 -0300

    23/05/2023 - 01:53

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class Main:
     def routine(self):
         self.hr()
         self.finance()
-        # self.sender()
+        self.flows()
         print('\nRoutine in {:.2f} seconds\n'.format(time() - self.start_time))
 
-Main().flows()
+Main().routine()
