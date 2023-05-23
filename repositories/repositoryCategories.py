ebdd6313d55097399a2c9e963f2da9975991b5ed
Caio Bretas <caiodbretas@icloud.com>
2023-05-23 01:45:39 -0300

    23/05/2023 - 01:45

--- repositories/repositoryCategories.py
+++ repositories/repositoryCategories.py
@@ -46,27 +46,28 @@ class RepositoryCategories( RepositoryBase ):
             return None
         
     def getCategories_fromExcel(self) -> list[Category] | None:
-        df: DataFrame = super().openDataFrame()            
-        try:
-            df.fillna(value="", inplace=True)
-            list_aux: list[Category] = []
-            for index, row in df.iterrows():
-                row = Category(
-                    id = row['id'],
-                    recorrencia= row['recorrência'],
-                    projeto = row['projeto'],
-                    produto = row['produto'],
-                    method_id = row['method_id'],
-                    subcategoria4 = row['subcategoria4'],
-                    subcategoria3 = row['subcategoria3'],
-                    subcategoria2 = row['subcategoria2'],
-                    subcategoria = row['subcategoria'],
-                    categoria = row['categoria'],
-                    categoriaprojecao = row['categoriaprojecao'],
-                    categoriacustoreceita= row['categoriacustoreceita']
-                    )
-                list_aux.append(row)
-            return list_aux
-        except Exception as e:
-            logging.error(f'Erro: {e}')
-            return None   
+        df: DataFrame = super().openDataFrame()
+        if not df.empty:       
+            try:
+                df.fillna(value="", inplace=True)
+                list_aux: list[Category] = []
+                for index, row in df.iterrows():
+                    row = Category(
+                        id = row['id'],
+                        recorrencia = row['recorrência'],
+                        projeto = row['projeto'],
+                        produto = row['produto'],
+                        method_id = row['method_id'],
+                        subcategoria4 = row['subcategoria4'],
+                        subcategoria3 = row['subcategoria3'],
+                        subcategoria2 = row['subcategoria2'],
+                        subcategoria = row['subcategoria'],
+                        categoria = row['categoria'],
+                        categoriaprojecao = row['categoriaprojecao'],
+                        categoriacustoreceita= row['categoriacustoreceita']
+                        )
+                    list_aux.append(row)
+                return list_aux
+            except Exception as e:
+                logging.error(f'Erro: {e}')
+                return None
