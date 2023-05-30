b37a6beb8c9690ccd43514cd8cad3a06900c4467
caiobretas <caiodinizbretas2001@gmail.com>
2023-05-30 12:16:26 -0300

    30/05/2023 = 12:16

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class Main:
         
         print('\nRoutine in {:.2f} seconds\n'.format(time() - self.start_time))
         
-# Main().routine()
+Main().routine()
 # Main().admin()
 # Main().emailrequests()
 # Main().hr()
