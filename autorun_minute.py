f298bdfa2f497ab09442f576d666de19d1d68db6
Caio Bretas <caiodbretas@icloud.com>
2023-06-01 19:06:34 -0300

    '01/06/2023'

---
+++ autorun_minute.py
@@ -0,0 +1,18 @@
+from main import Main
+from business.updateBook import UpdateBook
+from business.UpdateContacts import UpdateContacts
+from business.updateCategories import UpdateCategories
+from business.updateFutures import UpdateFutures
+
+
+main = Main()
+connection = main.connection
+engine = main.engine
+
+UpdateBook(connection, engine).update()
+UpdateContacts(connection, engine).update()
+UpdateCategories(connection, engine).update()
+UpdateFutures(connection, engine).update()
+
+main.admin()
+main.flows()
