19fe7021a947bdf01893892cd68968570d46d895
Caio Bretas <caiodbretas@icloud.com>
2023-06-13 22:24:38 -0300

    13/06/2023 - 22:24

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ class Main:
         self.admin()
         self.flows()
         
-        print('\nRoutine in {:.2f} seconds\n'.format(time() - self.start_time))
+        print('\nRoutine in {:.2f} seconds\n'.format(time() - self.start_time))        
