a6ca49444f0056aea61cba5e31b49705e5c8b3e1
Caio Bretas <caiodbretas@icloud.com>
2023-06-16 20:49:03 -0300

    ok

--- business/atualizaRepository.py
+++ business/atualizaRepository.py
@@ -15,10 +15,10 @@ class FinanceRepository:
     def update(self): 
         timer = time()
         
-        # UpdateTransactions(self.connection, self.engine).update()
-        # UpdateBook(self.connection,self.engine).update() # update book repository from 'interface.xlsx'
-        # UpdateCategories(self.connection, self.engine).update()
-        # UpdateCryptoTransactions(self.connection, self.engine).update()
+        UpdateTransactions(self.connection, self.engine).update()
+        UpdateBook(self.connection,self.engine).update() # update book repository from 'interface.xlsx'
+        UpdateCategories(self.connection, self.engine).update()
+        UpdateCryptoTransactions(self.connection, self.engine).update()
         UpdateCryptoPrices(self.connection,self.engine).update()
         UpdateKaminoTransactions(self.connection,self.engine).update()
         UpdateFutures(self.connection,self.engine).update()
