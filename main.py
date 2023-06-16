f5f646ea222b172ea7a730402091393844249b6b
Caio Bretas <caiodbretas@icloud.com>
2023-06-16 00:09:42 -0300

    opo

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ class Main:
         from business.updateProjection import UpdateProjection
         from business.finance.ExchangeVariationRate import ExchangeVariationRate
         
-        # FinanceRepository(self.connection,self.engine).update()
+        FinanceRepository(self.connection,self.engine).update()
         ExchangeVariationRate(self.connection,self.engine).updateSheet()
-        # UpdateProjection(self.connection,self.engine).update()
+        UpdateProjection(self.connection,self.engine).update()
         
     def hr(self):
         from business.UpdateContacts import UpdateContacts
@@ -84,5 +84,3 @@ class Main:
         self.flows()
     
         print('\nRoutine in {:.2f} seconds\n'.format(time() - self.start_time))
-        
-Main().finance()
