a4c55e73209ca0415d39d0914d6df0154708d94f
Caio Bretas <caiodbretas@icloud.com>
2023-06-20 21:30:12 -0300

    ok

--- autorun_day.py
+++ autorun_day.py
@@ -10,29 +10,29 @@ sender = GoogleGmail()
 from_ = 'caio.bretas@lumxstudios.com'
 to = 'caiodbretas@icloud.com'
 errorsubject = 'Error running script!'
-class AutorunDay: 
+# class AutorunDay: 
     
-    list_errors: list[Exception] = []
+#     list_errors: list[Exception] = []
     
-    @staticmethod
-    def run():    
-        try:
-            main.emailrequests()
-        except Exception as e:
-            draft: dict = sender.createDraft(from_=from_,to=to,subject=errorsubject,message_body=f'Error: {e}')
-            sender.sendDraft(draft.get('id', None))
-            AutorunDay.list_errors.append(e)
+#     @staticmethod
+#     def run():    
+#         try:
+#             main.emailrequests()
+#         except Exception as e:
+#             draft: dict = sender.createDraft(from_=from_,to=to,subject=errorsubject,message_body=f'Error: {e}')
+#             sender.sendDraft(draft.get('id', None))
+#             AutorunDay.list_errors.append(e)
             
-    @staticmethod
-    def sender():
-        subject = 'Successfully completed hour routine'
-        message = 'The following tasks were executed:\nInvoiceRequest\nReminderRequest'
-        draft: dict = sender.createDraft(from_=from_,to=to,subject=subject, message_body=message)
-        sender.sendDraft(draft.get('id', None))
+#     @staticmethod
+#     def sender():
+#         subject = 'Successfully completed hour routine'
+#         message = 'The following tasks were executed:\nInvoiceRequest\nReminderRequest'
+#         draft: dict = sender.createDraft(from_=from_,to=to,subject=subject, message_body=message)
+#         sender.sendDraft(draft.get('id', None))
         
-AutorunDay.run()
-AutorunDay.sender()
-print('Auto Run Day Complete')
+# AutorunDay.run()
+# AutorunDay.sender()
+# print('Auto Run Day Complete')
 
 
-main.flows()
+# main.flows()
